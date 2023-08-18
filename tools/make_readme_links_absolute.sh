f61db60cb6400137cab0cc7fa176403ba3fa6b58
Adam Lugowski <alugowski@gmail.com>
2023-08-18 16:42:14 -0700

    Make notebook links go to nbviewer

--- tools/make_readme_links_absolute.sh
+++ tools/make_readme_links_absolute.sh
@@ -2,4 +2,4 @@
 
 cat $(dirname "$0")/../README.md |
 sed -e 's,doc/,https://github.com/alugowski/matspy/blob/main/doc/,g' |
-sed -e 's,(demo-,(https://github.com/alugowski/matspy/blob/main/demo-,g'
+sed -e 's,(demo-,(https://nbviewer.org/github/alugowski/matspy/blob/main/demo-,g'
