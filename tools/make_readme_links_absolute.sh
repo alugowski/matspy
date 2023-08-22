a51441fbc3ec6be4462ac3d4d8af9c6156f6fbe7
Adam Lugowski <alugowski@gmail.com>
2023-08-22 15:54:25 -0700

    Fix `demo.ipynb` link on PyPI

--- tools/make_readme_links_absolute.sh
+++ tools/make_readme_links_absolute.sh
@@ -2,4 +2,4 @@
 
 cat $(dirname "$0")/../README.md |
 sed -e 's,doc/images/,https://raw.githubusercontent.com/alugowski/matspy/main/doc/images/,g' |
-sed -e 's,(demo-,(https://nbviewer.org/github/alugowski/matspy/blob/main/demo-,g'
+sed -e 's,(demo,(https://nbviewer.org/github/alugowski/matspy/blob/main/demo,g'
