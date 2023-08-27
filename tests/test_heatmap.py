cce1e368e7395662e47ba405d9eadfe366d7dfc4
Adam Lugowski <alugowski@gmail.com>
2023-08-26 21:33:42 -0700

    Add bucket tweak tests

--- tests/test_heatmap.py
+++ tests/test_heatmap.py
@@ -23,6 +23,32 @@ class SpyHeatmapTests(unittest.TestCase):
             heatmap = to_spy_heatmap(r, buckets=1, shading="binary")
             self.assertAlmostEqual(heatmap[0][0], 1.0, places=2)
 
+    def test_aa_tweaks(self):
+        from matspy.spy_renderer import _tweak_divisor
+
+        # more pixels than matrix dimensions
+        orig, display = 80, 100
+        self.assertEqual(_tweak_divisor(orig, display), orig)
+
+        orig, display = 100, 100
+        self.assertEqual(_tweak_divisor(orig, display), orig)
+
+        # matrix slightly bigger than screen
+        orig, display = 101, 100
+        self.assertEqual(_tweak_divisor(orig, display), orig)
+
+        orig, display = 100, 99
+        self.assertEqual(_tweak_divisor(orig, display), orig)
+
+        # matrix bigger than screen
+        orig, display = 415, 100
+        buckets = _tweak_divisor(orig, display)
+        self.assertLessEqual(orig % buckets, 2)
+
+        orig, display = 4315, 873
+        buckets = _tweak_divisor(orig, display)
+        self.assertLessEqual(orig % buckets, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
